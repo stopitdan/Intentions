bf7ed1d0e0bfcf7bc8be61cf1b7a600e591b4f19
Dan Wiegand <DanWiegand@Dans-MacBook-Pro.local>
2017-01-15 18:48:08 -0800

    fg

---
+++ Pods/Target Support Files/BWWalkthrough/BWWalkthrough-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "BWWalkthrough-BWWalkthrough-Bridging-Header.h"
+
+FOUNDATION_EXPORT double BWWalkthroughVersionNumber;
+FOUNDATION_EXPORT const unsigned char BWWalkthroughVersionString[];
+
